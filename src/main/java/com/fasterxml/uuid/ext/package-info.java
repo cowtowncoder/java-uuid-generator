80512a15568243c775a6076cff52f9d5d29bdd92
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-01-07 19:02:33 -0800

    Yet some more javadocs tweaks

--- src/main/java/com/fasterxml/uuid/ext/package-info.java
+++ src/main/java/com/fasterxml/uuid/ext/package-info.java
@@ -1,5 +1,5 @@
 /**
-Package that contains optional Java UUID Generator classes, ones that:
-depend on optional external packages (like slf4j)
+Package that contains optional Java UUID Generator classes,
+ones that depend on optional external packages (like slf4j)
 */
 package com.fasterxml.uuid.ext;
