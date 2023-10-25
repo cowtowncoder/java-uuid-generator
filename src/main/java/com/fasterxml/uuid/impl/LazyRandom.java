a892069c3e2684ed54beb019a5f598a7fcad6f44
Maia Everett <maia@everett.one>
2023-10-25 06:50:29 +0300

    Use in-code lazy initialization for LazyRandom (for compatibility with native code generation tools) (#85)

--- src/main/java/com/fasterxml/uuid/impl/LazyRandom.java
+++ src/main/java/com/fasterxml/uuid/impl/LazyRandom.java
@@ -9,9 +9,26 @@ import java.security.SecureRandom;
  */
 public final class LazyRandom
 {
-    private final static SecureRandom shared = new SecureRandom();
+    private static final Object lock = new Object();
+    private static volatile SecureRandom shared;
 
     public static SecureRandom sharedSecureRandom() {
-        return shared;
+        // Double check lazy initialization idiom (Effective Java 3rd edition item 11.6)
+        // Use so that native code generation tools do not detect a SecureRandom instance in a static final field.
+        SecureRandom result = shared;
+
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (lock) {
+            result = shared;
+
+            if (result == null) {
+                result = shared = new SecureRandom();
+            }
+
+            return result;
+        }
     }
 }
